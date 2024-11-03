a35efa4c3dd7e1376639746ea7539ad86a84506f
Satvik Umaanand Garimella <64387268+satvikgarimella@users.noreply.github.com>
2024-11-03 00:39:53 -0400

    Update WorkExpirences.js

--- src/WorkExpirences.js
+++ src/WorkExpirences.js
@@ -5,27 +5,18 @@ const WorkExperience = () => {
     <section id="work-experience" className="WorkExperience">
       <h2>Work Experience</h2>
       <div className="WorkExperience-list">
-        {/* Work Experience Card 1 */}
         <div className="WorkExperience-card">
           <h3>Full Stack Developer @ SaiDham Food Bank</h3>
           <p>
-            As a Full Stack Developer for a leading food bank initiative, I
-            designed a responsive, user-friendly website using React JS and CSS.
-            The backend, implemented using Node.js and Django, ensured seamless
-            integration, scalability, and security. My work optimized user
-            experience and maintained high availability across devices.
+            Designed a responsive, user-friendly website using React JS and CSS,
+            with a backend implemented in Node.js and Django.
           </p>
         </div>
-
-        {/* Work Experience Card 2 */}
         <div className="WorkExperience-card">
           <h3>Co-Lead @ Fx Hackathons</h3>
           <p>
-            As Co-Lead of a student-based club, I organized hackathons aimed at
-            exposing students with less programming experience to diverse
-            challenges. I facilitated meetings, prepared agendas, and developed
-            communication channels between teams, improving collaboration across
-            multiple projects.
+            Organized hackathons aimed at exposing students with less programming experience 
+            to diverse challenges.
           </p>
         </div>
       </div>
